20d4722ad9b7070d8d69dc17b2138ca44818ab83
Jasminejiamei <1121663274@qq.com>
2019-09-24 21:31:15 +0800

    修改图形界面

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
   
  http.createServer(function(req,res){
    res.writeHead(200,{"Content-Type":"text/html"});
-   res.write("<h1>Nodejs demo one</h1>");
-   res.end("<p>Hello World2</p>");
+   res.write("<p>The number of file`s letter is:</p>");
+   res.write("<p>The number of file`s word is:</p>");
+   res.write("<p>The number of file`s line is:</p>");
+   res.end("<button>Please select a file</button>");
 }).listen(3000);
 console.log("Http server is listening at port 3000");
