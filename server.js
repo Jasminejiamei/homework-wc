9eddd3135c7d89a7003afb984313a9aef27b70cb
Jasminejiamei <1121663274@qq.com>
2019-09-25 10:38:20 +0800

    修改高级功能

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
  var http = require("http");
+ var c = require('child_process');
   
  http.createServer(function(req,res){
    res.writeHead(200,{"Content-Type":"text/html"});
@@ -7,4 +8,5 @@
    res.write("<p>The number of file`s line is:</p>");
    res.end("<button>Please select a file</button>");
 }).listen(3000);
-console.log("Http server is listening at port 3000");
+// console.log("Http server is listening at port 3000");
+c.exec('start G:/aboutFrontEnd/前端/软工作业/homeworkWc/index.html')
